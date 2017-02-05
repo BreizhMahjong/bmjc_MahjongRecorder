68b783bc51a4447a049b4c37af288b322458ef6c
Kwoin <pierric.willemet@gmail.com>
2017-02-05 14:23:55 +0100

    use silex micro-framework

---
+++ views/view.php
@@ -0,0 +1,18 @@
+<!doctype html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <link href="microcms.css" rel="stylesheet" />
+    <title>MicroCMS - Home</title>
+</head>
+
+<body>
+    <header>
+        <h1>Breizh Mahjong Recorder</h1>
+    </header>
+
+    <p>test</p>
+
+</body>
+
+</html>
